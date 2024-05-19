f39c2f332f3eb68b6c2a2d65120843b9adeeb5af
Ze <elawrence.bda@gmail.com>
2024-05-19 03:21:37 -0400

    Fixed issue with auth.js

--- Develop/client/src/main.jsx
+++ Develop/client/src/main.jsx
@@ -1,10 +1,13 @@
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { ApolloProvider } from '@apollo/client';
+
 
 import App from './App.jsx'
 import SearchBooks from './pages/SearchBooks'
 import SavedBooks from './pages/SavedBooks'
+import { client } from './utils/apolloClient'
 
 const router = createBrowserRouter([
   {
@@ -24,5 +27,7 @@ const router = createBrowserRouter([
 ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <RouterProvider router={router} />
+  <ApolloProvider client={client}>
+    <RouterProvider router={router} />
+  </ApolloProvider>
 )
