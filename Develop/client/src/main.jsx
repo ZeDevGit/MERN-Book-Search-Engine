79798ab964d8e269561fecdd084a6f64bad22922
Ze <elawrence.bda@gmail.com>
2024-05-19 23:51:18 -0400

    Added fixes to jsx files

--- Develop/client/src/main.jsx
+++ Develop/client/src/main.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
@@ -29,5 +30,5 @@ const router = createBrowserRouter([
 ReactDOM.createRoot(document.getElementById('root')).render(
   <ApolloProvider client={client}>
     <RouterProvider router={router} />
-  </ApolloProvider>
+    </ApolloProvider>
 )
--- Develop/server/server.js
+++ Develop/server/server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const { ApolloServer } = require('@apollo/server');
 const { expressMiddleware } = require('@apollo/server/express4');
-const path = require('path');
 const { authMiddleware } = require('./utils/auth');
 const { typeDefs, resolvers } = require('./schemas');
+const path = require('path');
+
 const db = require('./config/connection');
 const cors = require('cors');
 
